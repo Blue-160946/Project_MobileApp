e846ef7dde748abb1d70c2d4413aa11634533899
Blue-160946 <blueblue160946@gmail.com>
2024-10-07 20:51:13 +0700

    fork code

---
+++ lib/main.dart
@@ -0,0 +1,69 @@
+import 'package:league_of_legends_universe/screens/form_screen.dart';
+import 'package:league_of_legends_universe/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:league_of_legends_universe/provider/transaction_provider.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) {
+          return TransactionProvider();
+        }),
+      ],
+      child: MaterialApp(
+        title: 'League of Legends Universe',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromRGBO(10, 50, 60, 1)),
+          useMaterial3: true,
+        ),
+        home: const MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Provider.of<TransactionProvider>(context, listen: false).initData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          body: TabBarView(
+            children: [
+              HomeScreen(),
+              FormScreen(),
+            ],
+          ),
+          bottomNavigationBar: TabBar(
+            tabs: [
+              Tab(text: "รายการธุรกรรม", icon: Icon(Icons.list),),
+              Tab(text: "เพิ่มข้อมูล", icon: Icon(Icons.add),),
+            ],
+          ),
+        ));
+  }
+}
